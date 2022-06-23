3f45f9d30da2b5591ad1bc5161f70292bf3add32
ebschult <ebschult@ncsu.edu>
2022-06-23 15:12:46 -0400

    Made the emulator a package

--- Assets/Scripts/CameraArrayController.cs
+++ Assets/Scripts/CameraArrayController.cs
@@ -4,9 +4,13 @@ using UnityEngine;
 
 public class CameraArrayController : MonoBehaviour
 {
+    // Horizontal movement speed
     public float walkingSpeed = 7.5f;
+    // Speed of rotation
     public float lookSpeed = 2.0f;
+    // Lock how far you can move in one motion
     public float lookXLimit = 45.0f;
+    // Vertical movement speed
     public float verticalSpeed = 5.0f;
 
     CharacterController characterController;
@@ -18,9 +22,10 @@ public class CameraArrayController : MonoBehaviour
 
     void Start()
     {
+        //Get the character controller
         characterController = GetComponent<CharacterController>();
 
-        // Lock cursor
+        // Lock and hide cursor
         Cursor.lockState = CursorLockMode.Locked;
         Cursor.visible = false;
     }
@@ -30,15 +35,13 @@ public class CameraArrayController : MonoBehaviour
         // We are grounded, so recalculate move direction based on axes
         Vector3 forward = transform.TransformDirection(Vector3.forward);
         Vector3 right = transform.TransformDirection(Vector3.right);
-        // Press Left Shift to run
-        bool isRunning = Input.GetKey(KeyCode.LeftShift);
+
+        // Calculate velocity based on the directional inputs of the player
         float curSpeedX = canMove ? walkingSpeed * Input.GetAxis("Vertical") : 0;
         float curSpeedY = canMove ? walkingSpeed * Input.GetAxis("Horizontal") : 0;
         float curSpeedZ = canMove ? verticalSpeed * Input.GetAxis("Jump") : 0;
-        float movementDirectionY = moveDirection.y;
         moveDirection = (forward * curSpeedX) + (right * curSpeedY);
         moveDirection.y = curSpeedZ;
-
 
         // Move the controller
         characterController.Move(moveDirection * Time.deltaTime);
